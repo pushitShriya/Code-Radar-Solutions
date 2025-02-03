f9667d454f609d7729110d7bb37d697e710c035a
Shriya <Shriyaasalaria@gmail.com>
2025-02-03 17:09:19 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -3,7 +3,7 @@ int main(){
     int num1, num2;
     scanf("%d" , &num1 , &num2);
     if (num1 > num2){
-        printf("d",num1);
+        printf("%d",num1);
     }
     else{
     printf("%d",num2);
